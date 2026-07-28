0a396da85728550aecd81ec7ed2f52c3ea31f0e0
Adryan <adr17yan@protonmail.com>
2026-07-28 17:03:57 +0000

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,7 +1,7 @@
 const USERS = [
   { username: "mimmarcelo", password: "Teste123" },
-  { username: "Udibone", password: "Antonny1" },
-  { username: "Tontoro", password: "Bento2" },
+  { username: "Udibone", password: "Bento2" },
+  { username: "Tontoro", password: "Antonny1" },
   { username: "Nyx", password: "Gervásio3" },
   { username: "root", password: "root" }
 ]
